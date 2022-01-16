e9704ed05e3be4eb7df48c9341fc7d9bd5c8eb45
Yiran WANG (Elaine) <ywanglf@connect.ust.hk>
2022-01-16 19:35:10 +0800

    Done :P

--- src/main/launchBrowser.java
+++ src/main/launchBrowser.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -13,13 +12,13 @@ import java.util.List;
 
 public class launchBrowser {
     private static int count = 0;
-    public static int MAX_PAGE = 40;
+    public static int MAX_PAGE = 100;
     public static WebDriver driver = null;
     public static FileWriter writer;
 
     static {
         try {
-            writer = new FileWriter("searchResult.csv",true);
+            writer = new FileWriter("Result_20220116.csv");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -39,7 +38,7 @@ public class launchBrowser {
 
     public static void directToURL(int page) throws IOException {
         ++count;
-        System.out.println("count: "+count);
+        System.out.println("page: "+page);
         if (count == 20) {
             count = 0;
             driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(2));
@@ -48,11 +47,18 @@ public class launchBrowser {
             driver.get("https://github.com/search?o=desc&p="+page+"&q=import+spoon.processing&s=indexed&type=Code");
         else
             driver.get("https://github.com/search?o=desc&q=import+spoon.processing&s=indexed&type=Code");
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
         //driver.navigate().refresh();
+       // driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
         //get and save href
 
         List<WebElement> webElementList = driver.findElements(By.cssSelector(".f4.text-normal"));
+
+        while (webElementList.size() <= 2) {
+            System.out.println(">> Empty ");
+            driver.navigate().refresh();
+            webElementList = driver.findElements(By.cssSelector(".f4.text-normal"));
+        }
         for (var i:webElementList){
             String url = i.findElement(By.tagName("a")).getAttribute("href");
             System.out.println(url);
@@ -60,6 +66,7 @@ public class launchBrowser {
             stringBuilder.append("\n");
         }
         writer.write(stringBuilder.toString());
+        stringBuilder = new StringBuilder();
     }
 
 
