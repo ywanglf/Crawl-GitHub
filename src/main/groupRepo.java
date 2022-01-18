99be98569b26130d3e67fd771c7a80dc6da554b0
Yiran WANG (Elaine) <ywanglf@connect.ust.hk>
2022-01-18 23:30:56 +0800

    Document and Code Update :P

--- src/main/groupRepo.java
+++ src/main/groupRepo.java
@@ -1,7 +1,5 @@
 package main;
 
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvValidationException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
